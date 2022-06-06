b9f007634af3f60642d6e301fc41b5819548ca40
Simon <zou-simon@hotmail.fr>
2022-06-06 21:25:12 +0200

    Add start button

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
 	import Flashcards from './Flashcards.svelte';
+
+	let start = false;
 </script>
 
 <main>
+	{#if start}
 	<Flashcards />
+	{:else}
+	<button on:click={() => start = true}>Start</button>
+	{/if}
 </main>
