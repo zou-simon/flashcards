944ece935fedf735766ac7185fd47b3e96db583a
Simon <zou-simon@hotmail.fr>
2022-06-06 18:35:56 +0200

    Add deck of cards

---
+++ src/Deck.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { words } from './words.js';
+  import Card from "./Card.svelte";
+
+  let numberCards = 10;
+  let showCardBack = false;
+  const toggleShowBack = () => showCardBack = !showCardBack;
+  let word = words[Math.floor(Math.random() * words.length)];
+
+  function changeCard() {
+    numberCards--;
+    if (numberCards > 0) {
+      showCardBack = false;
+      word = words[Math.floor(Math.random() * words.length)];
+      console.log(word)
+    }
+  }
+</script>
+
+{#if numberCards > 0}
+<Card {word} {showCardBack} />
+  {#if !showCardBack}
+  <button on:click={toggleShowBack}>Return card</button>
+  {:else}
+  <button on:click={changeCard}>Next card</button>
+  {/if}
+{/if}
