2d3df81d88f84a9b6fb53ff6ca337d0f0badc0ee
Simon <zou-simon@hotmail.fr>
2022-06-06 23:47:24 +0200

    Add styles on card

--- src/Card.svelte
+++ src/Card.svelte
@@ -2,7 +2,7 @@
     import { onMount } from "svelte";
     import { apiData, definitions, answerData } from "./store.js";
 
-    export let word = '';
+    export let word = "";
     export let showCardBack = false;
 
     onMount(async () => {
@@ -18,15 +18,63 @@
 </script>
 
 {#if !showCardBack}
-    {word}
+    <h1>Question</h1>
+
+    <div class="card">
+        <p>
+            What is/are the definition(s) of <span class="bold">{word}</span>?
+        </p>
+    </div>
 {:else}
-    {word}
+    <h1>Answer</h1>
+
+    <p>Definition of {word}</p>
     <ul>
         {#each $definitions as definition, key}
             <li>
-                <input type="checkbox" bind:group={$answerData} name={word} value={word + '-' + key} />
+                <input
+                    type="checkbox"
+                    bind:group={$answerData}
+                    name={word}
+                    value={word + "-" + key}
+                />
                 {definition}
             </li>
         {/each}
     </ul>
 {/if}
+
+<style>
+    h1 {
+        text-align: center;
+        padding: 2em 0em 1em 0;
+        margin: 5px;
+        color: black;
+        text-transform: uppercase;
+        font-size: 3em;
+    }
+
+    .card {
+        background-color: #e7ed9f;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        width: 350px;
+        padding: 4em;
+        margin: 20px auto;
+    }
+
+    p {
+        color: black;
+        font-size: 2em;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .bold {
+        font-weight: bold;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+</style>
