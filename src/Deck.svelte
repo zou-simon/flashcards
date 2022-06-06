53d4a9d353874601e9a68d7005903a8645fe0e54
Simon <zou-simon@hotmail.fr>
2022-06-06 20:18:31 +0200

    Divide the words into 3 groups

--- src/Deck.svelte
+++ src/Deck.svelte
@@ -2,13 +2,14 @@
   import { words } from './words.js';
   import Card from "./Card.svelte";
 
+  export let deckNumber = 0;
   let numberCards = 10;
   let showCardBack = false;
   const toggleShowBack = () => showCardBack = !showCardBack;
   let word = getRandomWord();
 
   function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
+    return words[deckNumber][Math.floor(Math.random() * words[deckNumber].length)];
   }
 
   function changeCard() {
