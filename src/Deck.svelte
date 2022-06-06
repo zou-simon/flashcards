f808c963733dc7f0d630816b6391192a631d93f0
Simon <zou-simon@hotmail.fr>
2022-06-06 22:57:55 +0200

    Remove numberCards and reformat code

--- src/Deck.svelte
+++ src/Deck.svelte
@@ -1,31 +1,27 @@
 <script>
-  import { words } from './words.js';
+  import { words } from "./words.js";
   import Card from "./Card.svelte";
 
   export let deckNumber = 0;
-  let numberCards = 10;
   let showCardBack = false;
-  const toggleShowBack = () => showCardBack = !showCardBack;
+  const toggleShowBack = () => (showCardBack = !showCardBack);
   let word = getRandomWord();
 
   function getRandomWord() {
-    return words[deckNumber][Math.floor(Math.random() * words[deckNumber].length)];
+    return words[deckNumber][
+      Math.floor(Math.random() * words[deckNumber].length)
+    ];
   }
 
   function changeCard() {
-    numberCards--;
-    if (numberCards > 0) {
-      showCardBack = false;
-      word = getRandomWord();
-    }
+    showCardBack = false;
+    word = getRandomWord();
   }
 </script>
 
-{#if numberCards > 0}
 <Card {word} {showCardBack} />
-  {#if !showCardBack}
+{#if !showCardBack}
   <button on:click={toggleShowBack}>Return card</button>
-  {:else}
+{:else}
   <button on:click={changeCard}>Next card</button>
-  {/if}
 {/if}
