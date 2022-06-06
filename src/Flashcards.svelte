2109d86d63d04828cc2ec4adc9fee12b1d17821f
Simon <zou-simon@hotmail.fr>
2022-06-06 23:28:14 +0200

    Add style on decks page

--- src/Flashcards.svelte
+++ src/Flashcards.svelte
@@ -1,18 +1,86 @@
 <script>
-    import { selectedDeck } from './store.js';
-    import Deck from './Deck.svelte';
+    export let src = "./pointed-star.png";
+    import { selectedDeck } from "./store.js";
+    import Deck from "./Deck.svelte";
 
     let selected;
 
-    selectedDeck.subscribe(value => {
-		selected = value;
-	});
+    selectedDeck.subscribe((value) => {
+        selected = value;
+    });
 </script>
 
 {#if selected != undefined}
-<Deck deckNumber={selected} />
+    <Deck deckNumber={selected} />
 {:else}
-<button on:click={() => selectedDeck.set(0)}>1</button>
-<button on:click={() => selectedDeck.set(1)}>2</button>
-<button on:click={() => selectedDeck.set(2)}>3</button>
+    <h1>Select a deck of cards</h1>
+
+    <div class="desks">
+        <div class="desk deck1" on:click={() => selectedDeck.set(0)}>
+            <img {src} alt="deck 1" />
+            <h2>Deck 1</h2>
+        </div>
+        <div class="desk deck2" on:click={() => selectedDeck.set(1)}>
+            <img {src} alt="deck 2" />
+            <h2>Deck 1</h2>
+        </div>
+        <div class="desk deck3" on:click={() => selectedDeck.set(2)}>
+            <img {src} alt="deck 3" />
+            <h2>Deck 1</h2>
+        </div>
+    </div>
 {/if}
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    div .desks {
+        display: flex;
+        text-align: center;
+        padding: 2em;
+        margin: 20px;
+    }
+
+    div.desks .desk {
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        padding: 4em;
+        margin: 20px;
+    }
+    div.desks .deck1 {
+        background-color: #e5baba;
+    }
+    div.desks .deck2 {
+        background-color: #89d3eb;
+    }
+    div.desks .deck3 {
+        background-color: #8580bf;
+    }
+    img {
+        width: 50%;
+        padding: 30px;
+        margin-top: 20px;
+    }
+    h1 {
+        color: black;
+        text-transform: uppercase;
+        font-size: 3em;
+    }
+
+    h2 {
+        color: black;
+        font-size: 2em;
+        padding: 20px;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
